af7668509e7616778c75c93011abea0aa1dc9bae
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-06-09 11:40:44 +0500

    used getattr

--- record.py
+++ record.py
@@ -33,50 +33,39 @@ class Record:
         events -- list of weather related eventsax
         wind_direction -- unit (degrees
         """
-        self.date = datetime.strptime(
-            ' '.join(date.split('-')), "%Y %m %d").date()
+        self.date = datetime.strptime(' '.join(date.split('-')), "%Y %m %d").date()
+        self.max_temp = int(max_temp) if max_temp.isdigit() else 0
+        self.mean_temp = int(mean_temp) if mean_temp.isdigit() else 0
+        self.min_temp = int(min_temp) if min_temp.isdigit() else 0
+        self.max_dew = int(max_dew) if max_temp.isdigit() else 0
+        self.mean_dew = int(mean_dew) if mean_temp.isdigit() else 0
+        self.min_dew = int(min_dew) if min_temp.isdigit() else 0
+        self.max_humidity = int(max_humidity) if max_humidity.isdigit() else 0
+        self.mean_humidity = int(mean_humidity) if mean_humidity.isdigit() else 0
+        self.min_humidity = int(min_humidity) if max_humidity.isdigit() else 0
+        self.max_sea_pressure = int(max_sea_pressure)
+        self.mean_sea_pressure = int(mean_sea_pressure)
+        self.min_sea_pressure = int(min_sea_pressure)
+        self.max_visibility = int(max_visibility) if max_visibility.isdigit() else 0
+        self.mean_visibility = int(mean_visibility) if mean_visibility.isdigit() else 0
+        self.min_visibility = int(min_visibility) if min_visibility.isdigit() else 0
         self.max_wind_speed = int(max_wind_speed)
         self.mean_wind_speed = int(mean_wind_speed)
-        self.max_gust_speed = int(
-            max_gust_speed) if max_gust_speed.isdigit() else 0
+        self.max_gust_speed = int(max_gust_speed) if max_gust_speed.isdigit() else 0
         self.precipitation = float(precipitation)
         self.cloud_cover = int(cloud_cover) if cloud_cover.isdigit() else 0
         self.events = events.split('-')
-        self.wind_direction = int(
-            wind_direction) if wind_direction.isdigit() else 0
-        self.data_dict = {
-            'date': self.date,
-            'max_temp': int(max_temp) if max_temp.isdigit() else 0,
-            'mean_temp': int(mean_temp) if mean_temp.isdigit() else 0,
-            'min_temp': int(min_temp) if min_temp.isdigit() else 0,
-            'max_dew': int(max_dew) if max_temp.isdigit() else 0,
-            'mean_dew':  int(mean_dew) if mean_temp.isdigit() else 0,
-            'min_dew': int(min_dew) if min_temp.isdigit() else 0,
-            'max_humidity': int(max_humidity) if max_humidity.isdigit() else 0,
-            'mean_humidity':  int(mean_humidity) if mean_humidity.isdigit() else 0,
-            'min_humidity': int(min_humidity) if max_humidity.isdigit() else 0,
-            'max_sea_pressure': int(max_sea_pressure),
-            'mean_sea_pressure': int(mean_sea_pressure),
-            'min_sea_pressure': int(min_sea_pressure),
-            'max_visibility': int(max_visibility) if max_visibility.isdigit() else 0,
-            'mean_visibility': int(mean_visibility) if mean_visibility.isdigit() else 0,
-            'min_visibility': int(min_visibility) if min_visibility.isdigit() else 0
-        }
-
+        self.wind_direction = int(wind_direction) if wind_direction.isdigit() else 0
     def __iter__(self):
         """ iterator method """
         return iter([self.date,
-                     self.data_dict['max_temp'], self.data_dict['mean_temp'],
-                     self.data_dict['min_temp'], self.data_dict['max_dew'],
-                     self.data_dict['mean_dew'], self.data_dict['min_dew'],
-                     self.data_dict['max_humidity'], self.data_dict['mean_humidity'],
-                     self.data_dict['min_humidity'], self.data_dict['max_sea_pressure'],
-                     self.data_dict['mean_sea_pressure'], self.data_dict['min_sea_pressure'],
-                     self.data_dict['max_visibility'], self.data_dict['mean_visibility'],
-                     self.data_dict['min_visibility'], self.max_wind_speed,
-                     self.mean_wind_speed, self.max_gust_speed,
+                     self.max_temp, self.mean_temp, self.min_temp,
+                     self.max_dew, self.mean_dew, self.min_dew,
+                     self.max_humidity, self.mean_humidity, self.min_humidity,
+                     self.max_sea_pressure, self.mean_sea_pressure, self.min_sea_pressure,
+                     self.max_visibility, self.mean_visibility, self.min_visibility,
+                     self.max_wind_speed, self.mean_wind_speed, self.max_gust_speed,
                      self.precipitation, self.cloud_cover, self.events, self.wind_direction])
-
 
 """ Utility functions """
 
